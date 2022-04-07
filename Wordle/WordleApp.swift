a55b665c5a7bb88ac39be36d9328d2a7b0f124aa
Brady Robshaw <heybrady88@gmail.com>
2022-04-07 11:40:46 -0400

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Brady Robshaw on 4/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
