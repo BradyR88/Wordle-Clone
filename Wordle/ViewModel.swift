462b9d4bfffee7ff1b136ab35fb8d3faa4fc2c09
Brady Robshaw <heybrady88@gmail.com>
2022-04-08 21:27:23 -0400

    - add color changing letters logic

--- Wordle/ViewModel.swift
+++ Wordle/ViewModel.swift
@@ -43,12 +43,14 @@ class ViewModel: ObservableObject {
         for tile in tiles {
             if tile.coordinates == coordinates {
                 switch tile.status {
-                case .blank, .rong:
+                case .blank:
                     return .clear
                 case .rightSpot:
                     return .green
                 case .rightWrongSpot:
                     return .yellow
+                case .rong:
+                    return .gray
                 }
             }
         }
@@ -84,7 +86,7 @@ class ViewModel: ObservableObject {
                 tiles[index].leter = ""
             }
             else {
-                print("Something has gone terribly wrong. =( Check your backspace button! your cursor position might not be where you expect it to be!")
+                print("Something has gone terribly wrong. =( Check your backspace button func! Your cursor position might not be where you expect it to be!")
             }
         }
     }
@@ -94,10 +96,46 @@ class ViewModel: ObservableObject {
         if let index = self.tiles.firstIndex(where: { $0.coordinates == cursorPosition }) {
             
             if cursorPosition.column == 5 && tiles[index].leter != "" /* && isARealWorld == true */ {
+                checkLine() // this Hass to be done before we move to the next row because it's getting its row information from the current cursor position
                 cursorPosition.column = 1
                 
                 if cursorPosition.row != 6 {
                     cursorPosition.row += 1
+                }
+            }
+        }
+    }
+    
+    // this has to be done before we move to the next row because it's getting its row information from the current cursor position
+    func checkLine() {
+        
+        var hasAVerticalMach = [false,false,false,false,false]
+        var hasAMach = [false,false,false,false,false]
+        
+        // vertical test for .rightSpot
+        for column in 1...5 {
+            if let index = self.tiles.firstIndex(where: { $0.coordinates == Coordinates(row: cursorPosition.row, column: column) }) {
+                if rightAnswer[column - 1].uppercased() == tiles[index].leter.uppercased() {
+                    tiles[index].status = .rightSpot
+                    hasAVerticalMach[column - 1] = true
+                }
+                else {
+                    tiles[index].status = .rong
+                }
+            }
+        }
+        // diagonal test for rightWrongSpot
+        for column in 1...5 {
+            if hasAVerticalMach[column - 1] == false {
+                if let index = self.tiles.firstIndex(where: { $0.coordinates == Coordinates(row: cursorPosition.row, column: column) }) {
+                    for letterInAnswer in 1...5 {
+                        if hasAVerticalMach[letterInAnswer - 1] == false || hasAVerticalMach[letterInAnswer - 1] == false {
+                            if tiles[index].leter.uppercased() == rightAnswer[letterInAnswer - 1] {
+                                tiles[index].status = .rightWrongSpot
+                                hasAMach[letterInAnswer - 1] = true
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -121,4 +159,11 @@ enum SubmissionStatus {
     case rightWrongSpot
     case rong
     case blank
+}
+
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
 }
